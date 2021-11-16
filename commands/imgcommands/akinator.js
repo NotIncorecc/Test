d7b5168ab92020e88eda942aa1229b4c27a482f5
NotIncorecc <85992147+NotIncorecc@users.noreply.github.com>
2021-11-16 12:50:06 +0530

    akinator error
    akinator-js malfunctioning

--- commands/imgcommands/akinator.js
+++ commands/imgcommands/akinator.js
@@ -3,6 +3,11 @@ module.exports = {
     name:'akinator',
     description:'yes this is akinator',
     async execute(message, client) {
+        try{
         akinator(message, client);
+        }catch(err){
+            message.channel.send('there was an error please try again',err)
+            return
+        }
     }
 }    
